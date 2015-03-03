4e9a7819b94182cb5416e81c12462efd0da7e821
Geoff Winner & Reid Baldwin <student@epicodus.com>
2015-03-03 11:05:20 -0800

    update with letter

--- app/app.php
+++ app/app.php
@@ -3,8 +3,29 @@
 
     $app = new Silex\Application();
 
-    $app->get("/hello", function() {
-        return "Hello friend!";
+    $app->get("/", function() {
+        return
+        "<!DOCTYPE html>
+        <html>
+        <head>
+            <title>Hello Friend!</title>
+            <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>
+        </head>
+        <body>
+            <div class='container'>
+                <h1>Hello from afar</h1>
+                <p>Dear Friend,</p>
+                <p> How's it going? </p>
+                <p>cheers</p>
+                <p>travel dude</p>
+                <a href='/goodbye'> Goodbye </a>
+            </div>
+        </body>
+        </html>"
+        ;
+    });
+    $app->get("/goodbye", function() {
+        return "Goodbye friend!";
     });
 
     return $app;
